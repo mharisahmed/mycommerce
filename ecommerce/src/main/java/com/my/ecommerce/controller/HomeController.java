f292e4f11b14ddfd8347c513cd61a51375ff5db9
mharisahmed <m.harisahmed@hotmail.com>
2018-01-17 13:23:20 +0500

    url's not loading

---
+++ ecommerce/src/main/java/com/my/ecommerce/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.my.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/test")
+	public ModelAndView Index(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("message","welcome to the party!!!!");
+		return mv;
+	}
+}
